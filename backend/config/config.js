239c069739211f374855bf3d16bb2875d1cfc9e2
prathvirajpshetty <prathviraj.shetty@exathought.com>
2025-10-15 14:40:26 +0530

    Added dev and prod db

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'udupiyar',
+    database: process.env.DB_NAME || 'dev_udupiyar_operations', // Use dev database for localhost
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
@@ -19,7 +19,7 @@ module.exports = {
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.PROD_DB_NAME || 'prod_udupiyar_operations', // Use prod database for Lambda
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
